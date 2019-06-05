67699741ad55829a06c012d1a980cfe0bcc69df5
Shadrach Tuck <shad.tuck@yahoo.com>
2019-06-04 18:26:20 -0600

    making good progress

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ArticleController
+
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+
+    {
+        return new Response('Whaddup!?');
+    }
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+
+    {
+        return new Response(sprintf(
+            "Future page to show the article: %s", $slug
+            ));
+    }
+}
